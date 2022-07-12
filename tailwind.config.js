43a2e8f279d5d5e11c74339c06f118a1651e231a
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-07-12 22:09:44 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html"],
+    content: ["./index.html", "./**/index.js"],
     theme: {
         extend: {
             fontFamily: {
                 'poppins': ['Poppins', 'sans-serif']
             },
+            animation: {
+                fadeIn: "fadeIn 2s ease-in forwards"
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: 0 },
+                    "100%": { opacity: 1 }
+                }
+            },
         },
     },
+    variants: {
+        animation: ["motion-safe"]
+    },
     plugins: [],
+
 }
